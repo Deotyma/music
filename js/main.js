2f3910d42e205396d5d15ebdde74a1ae01104943
Ewa Kadziolka <kalistaewa@gmail.com>
2021-02-06 20:56:22 +0100

    start player

--- js/main.js
+++ js/main.js
@@ -2,38 +2,6 @@ const voice = document.querySelector(".voice");
 const voice2text = document.querySelector(".voice2text");
 const speak = document.querySelector("p.speak");
 
-var chansons = [
-  {
-    id: 1,
-    author: "Édith Piaf",
-    titre: "la vie en rose",
-    song: "../chansons/08EdithPiaf-NonJeNeRegretteRien-1960.mp3",
-    image: "../images/edith-piaf.jpg",
-  },
-  {
-    id: 2,
-    author: "Édith Piaf",
-    titre: "non je ne regrette rien",
-    song: "../chansons/08EdithPiaf-NonJeNeRegretteRien-1960.mp3",
-    image: "../images/edith-piaf.jpg",
-  },
-  {
-    id: 3,
-    author: "Charles Aznavour",
-    titre: "emmenez-moi",
-    song: "../chansons/01.Emmenez-moi  Charles AZNAVOUR_nohazik.co.mp3",
-    image: "../images/Charles-Aznavour.jpg",
-  },
-  {
-    id: 4,
-    author: "Charles Aznavour",
-    titre: "rendez-vous a brasilia",
-    song:
-      "../chansons/Charles Aznavour - Rendez-vous a Brasilia_nohazik.co.mp3",
-    image: "../images/Charles-Aznavour.jpg",
-  },
-];
-
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recorder = new SpeechRecognition();
